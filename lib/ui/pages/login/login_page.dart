4fc2a5cb33af2b77264ff9e6f1b14ea63ba5ccf8
Pedro Igor <pedrode13souza@gmail.com>
2022-02-21 10:47:00 -0300

    Add image on card

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../utils/images.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,8 +9,44 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(Images.logoApp),
+                Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                  elevation: 8,
+                  margin: const EdgeInsets.all(8),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(Images.mapEg),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Encontre entregadores \nna sua região!',
+                          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 24),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Utiliza o nosso Mapa de Localização \ne encontre encontre entregadores de \nacordo com a sua demanda',
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/ui/utils/images.dart
@@ -0,0 +1,5 @@
+//import '../assets/images/svg/map_eg.svg';
+class Images {
+  static const String logoApp = 'lib/ui/assets/images/png/logo.png';
+  static const String mapEg = 'lib/ui/assets/images/svg/map_eg.svg';
+}
