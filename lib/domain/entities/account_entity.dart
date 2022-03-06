50a04adc1a08156e218ff282c66da62e888236f5
Pedro Igor <pedrode13souza@gmail.com>
2022-03-06 19:09:19 -0300

    add Entities

---
+++ lib/domain/entities/account_entity.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class AccountEntity extends Equatable {
+  final String token;
+
+  const AccountEntity({required this.token});
+
+  @override
+  List get props => [token];
+}
---
+++ lib/domain/entities/entities.dart
@@ -0,0 +1,2 @@
+export 'account_entity.dart';
+export 'auth_result_entity.dart';
