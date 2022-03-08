b7bde8d21ee8b1f56ce3671e96b6d9822da321ea
Pedro Igor <pedrode13souza@gmail.com>
2022-03-08 16:48:10 -0300

    update login presenter

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,9 +1,17 @@
+import '../../helpers/helpes.dart';
+
 abstract class LoginPresenter {
+  Stream<UIError> get emailErrorStream;
+  Stream<UIError> get passwordErrorStream;
+  Stream<UIError> get mainErrorStream;
+  Stream<String> get navigateToStream;
   Stream<bool> get isFormValidStream;
   Stream<bool> get isLoadingStream;
+  Stream<int> get signUpTypeStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
+  void selectTypeSignUp(int type);
   void goToSignUp();
   void auth();
   void goToForgotPassword();
