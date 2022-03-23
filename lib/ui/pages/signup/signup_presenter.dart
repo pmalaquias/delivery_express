18eb10b91a505c36dc7ce6a900dac3a752a57f6e
Pedro Igor <pedrode13souza@gmail.com>
2022-03-22 21:15:59 -0300

    creating signup presenter with navigation functions

---
+++ lib/ui/pages/signup/signup_presenter.dart
@@ -0,0 +1,14 @@
+abstract class SignUpPresenter {
+  void cancelSignUp();
+
+  void goToAccessData();
+  void goToAddressData();
+  void goToVehicleData();
+  void goToReceiveData();
+  void goToFinish();
+
+  void backToPersonalData();
+  void backToAccessData();
+  void backToAddressData();
+  void backToVehicleData();
+}
