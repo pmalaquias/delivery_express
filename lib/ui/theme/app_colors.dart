b2cef34bcb5dde1bb9f47600da9009ef4faca3db
Pedro Igor <pedrode13souza@gmail.com>
2022-03-22 21:50:21 -0300

    move appColor to own file

---
+++ lib/ui/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+mixin AppColors {
+  static const Color black = Color(0xFF262626);
+  static const Color grey1 = Color(0xFF4C4C4C);
+  static const Color grey2 = Color(0xFFB1B1B1);
+  static const Color grey3 = Color(0xFFF3F3F3);
+  static const Color lightPurple = Color(0xFFF3F0F5);
+  static const Color purple = Color(0xFF712369);
+  static const Color heavyPurple = Color(0xFF4E1C51);
+  static const Color extraHeavyPurple = Color(0xFF25083E);
+  static const Color seccondRed = Color(0xFF7F0000);
+  //static const Color pink = Color(0xFFFFB0CF);
+  static const Color primaryRed = Color(0xFFB71C1C);
+  static const Color danger = Color(0xFFF26F66);
+  static const Color flatOrange = Color(0xFFFF8C64);
+  //static const Color flatYellow = Color(0xFFE5B022);
+  static const Color success = Color(0xFF3DC14E);
+  static const Color flatBlue = Color(0xFF66C7F2);
+  static const Color background = Color(0xFFFFFFFF);
+}
