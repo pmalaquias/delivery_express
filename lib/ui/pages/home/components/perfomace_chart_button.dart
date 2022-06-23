fb505d182b511d35d892dfbd04a8790d2ce820c6
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-06-23 15:51:24 -0300

    Perfomace chart button componet

---
+++ lib/ui/pages/home/components/perfomace_chart_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PerfomaceChartButton extends StatefulWidget {
+  const PerfomaceChartButton({Key? key}) : super(key: key);
+
+  @override
+  State<PerfomaceChartButton> createState() => _PerfomaceChartButtonState();
+}
+
+class _PerfomaceChartButtonState extends State<PerfomaceChartButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {},
+      icon: const Icon(Icons.bar_chart_rounded),
+      label: Text('Gráficos de Desempenho'.toUpperCase()),
+    );
+  }
+}
