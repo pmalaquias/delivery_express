3e6c2e957fdb68bc7c976d9f6d508fa08af17178
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-06-23 16:05:38 -0300

    component export files

---
+++ lib/ui/pages/home/components/components.dart
@@ -0,0 +1,7 @@
+export 'area_chart.dart';
+export 'custom_drawer.dart';
+export 'header.dart';
+export 'info_card.dart';
+export 'map.dart';
+export 'my_location_button_custtom.dart';
+export 'perfomace_chart_button.dart';
