728fc0341f5f101e06da3b2403423030f756712b
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-06-28 10:27:42 -0300

    Add Profile on custom Drawer

--- lib/ui/pages/home/components/custom_drawer.dart
+++ lib/ui/pages/home/components/custom_drawer.dart
@@ -1,6 +1,8 @@
+import 'package:delivery_express/ui/pages/profile/profile.dart';
 import 'package:flutter/material.dart';
 
 import '../../../theme/theme.dart' show AppColors;
+import '../../pages.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key? key}) : super(key: key);
@@ -33,7 +35,26 @@ class CustomDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.home_rounded),
             title: const Text('Início'),
-            onTap: () {},
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_rounded),
+            title: const Text('Perfil'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ProfilePage(),
+                ),
+              );
+            },
           ),
           ListTile(
             leading: const Icon(Icons.list_alt_rounded),
