0b2c23ca8c9deb063cb130534f788457bc301dd9
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-06-29 18:17:15 -0300

    Drawer custom client component

--- lib/ui/pages/home/components/custom_drawer.dart
+++ lib/ui/pages/home/components/custom_drawer.dart
@@ -5,8 +5,8 @@ import '../../../../l10n/strings/app_localizations.dart' show AppLocalizations;
 import '../../../theme/theme.dart' show AppColors;
 import '../../pages.dart';
 
-class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({Key? key}) : super(key: key);
+class CustomDrawerDeliveryMan extends StatelessWidget {
+  const CustomDrawerDeliveryMan({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -65,6 +65,96 @@ class CustomDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.payments_rounded),
             title: Text(AppLocalizations.of(context)!.drawerCompleteExtract),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings_rounded),
+            title: Text(AppLocalizations.of(context)!.drawerAccountConfigurations),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.help_rounded),
+            title: Text(AppLocalizations.of(context)!.drawerHelpAndSuport),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.info_rounded),
+            title: Text(AppLocalizations.of(context)!.drawerAbout),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app_rounded),
+            title: Text(AppLocalizations.of(context)!.drawerExit),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDrawerClient extends StatelessWidget {
+  const CustomDrawerClient({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                CircleAvatar(
+                  backgroundColor: AppColors.primaryRed,
+                  child: Icon(
+                    Icons.store_rounded,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  radius: 35,
+                ),
+                Text('Supermecado Nova Europa'),
+                Text('Av. JK, Ouro Preto - MG'),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_rounded),
+            title: Text(AppLocalizations.of(context)!.home),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_rounded),
+            title: Text(AppLocalizations.of(context)!.drawerProfile),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ProfilePage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.swap_calls_rounded),
+            title: const Text('Entregas em andamento'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.list_alt_rounded),
+            title: Text(AppLocalizations.of(context)!.drawerCompletedTransactions),
             onTap: () {},
           ),
           ListTile(
