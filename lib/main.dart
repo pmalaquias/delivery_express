8d98a62bb04a24b603833112c7d04a829bceb8ae
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-07-04 11:29:24 -0300

    🛠️ : Add dot anv  file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 
 import 'main/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+
+  FlutterConfig.get('GOOGLE_MAPS_API_KEY');
+
   runApp(const DeliveryExpressApp());
 }
