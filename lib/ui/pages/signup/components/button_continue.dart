04f1df05a9b3de0d372f0ebedb676001a916f8da
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-07-04 17:15:18 -0300

    creating ButtonContinuePushReplacement

--- lib/ui/pages/signup/components/button_continue.dart
+++ lib/ui/pages/signup/components/button_continue.dart
@@ -1,27 +1,37 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
+import '../../../extension/extension.dart';
+
 class ButtonContinue extends StatelessWidget {
-  final PageController controller;
-  final FutureOr<void> Function() func;
+  final Function() function;
+
   const ButtonContinue({
     Key? key,
-    required this.controller,
-    required this.func,
+    required this.function,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () async {
-        await func();
-        controller.nextPage(
-          duration: const Duration(milliseconds: 400),
-          curve: Curves.easeIn,
-        );
-      },
-      child: const Text('CONTINUAR'),
+      onPressed: () => function(),
+      child: Text(context.loc.continueButton.toUpperCase()),
+    );
+  }
+}
+
+class ButtonContinuePushReplacement extends StatelessWidget {
+  final Function(BuildContext context) function;
+
+  const ButtonContinuePushReplacement({
+    Key? key,
+    required this.function,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => function(context),
+      child: Text(context.loc.continueButton.toUpperCase()),
     );
   }
 }
