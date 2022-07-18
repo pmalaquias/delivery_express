7f9879cd05d5c7e4ff73d52ab1d526de286c1429
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-07-18 17:52:35 -0300

    custom radio button for delivery and vehicletype

--- lib/ui/pages/signup/components/radio_option_custom.dart
+++ lib/ui/pages/signup/components/radio_option_custom.dart
@@ -8,6 +8,9 @@ class RadioOptionCustom<T> extends StatelessWidget {
   final String text;
   final ValueChanged<T?> onChanged;
   final double imgSize;
+  final double selectImgSize;
+  final Color accentColor;
+  final double widthSizeCard;
 
   const RadioOptionCustom({
     required this.value,
@@ -16,16 +19,30 @@ class RadioOptionCustom<T> extends StatelessWidget {
     required this.text,
     required this.onChanged,
     this.imgSize = 20,
+    this.selectImgSize = 40,
+    this.accentColor = Colors.white,
+    this.widthSizeCard = 90,
     Key? key,
   }) : super(key: key);
 
   Widget _buildLabel() {
     final bool isSelected = value == groupValue;
 
-    return SvgPicture.asset(
-      text,
-      height: isSelected ? 40 : imgSize,
-      color: isSelected ? null : const Color.fromARGB(99, 158, 158, 158),
+    return SizedBox(
+      width: isSelected ? widthSizeCard : 40,
+      height: 50,
+      child: Card(
+        elevation: !isSelected ? 0 : 1.0,
+        color: isSelected ? accentColor : Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: SvgPicture.asset(
+            text,
+            height: isSelected ? selectImgSize : imgSize,
+            color: isSelected ? null : const Color.fromARGB(99, 158, 158, 158),
+          ),
+        ),
+      ),
     );
   }
 
@@ -43,6 +60,69 @@ class RadioOptionCustom<T> extends StatelessWidget {
               _buildLabel(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class RadioOptionTypeCustom<T> extends StatelessWidget {
+  final T value;
+  final T? groupValue;
+  final String label;
+  final String text;
+  final ValueChanged<T?> onChanged;
+  final double imgSize;
+
+  const RadioOptionTypeCustom({
+    required this.value,
+    required this.groupValue,
+    required this.label,
+    required this.text,
+    required this.onChanged,
+    this.imgSize = 35,
+    Key? key,
+  }) : super(key: key);
+
+  Widget _buildLabel() {
+    final bool isSelected = value == groupValue;
+
+    return Card(
+      color: isSelected ? const Color(0xFFFFDAD6) : const Color(0xFFF5DDDA),
+      child: SizedBox(
+        width: 112,
+        height: 95,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              text,
+              height: isSelected ? 40 : imgSize,
+              color: isSelected ? null : const Color(0xFF534341),
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? Colors.black : const Color(0xFF534341),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onChanged(value),
+      splashColor: Theme.of(context).primaryColor.withOpacity(0.1),
+      child: Padding(
+        padding: EdgeInsets.zero,
+        child: Row(
+          children: [
+            _buildLabel(),
+          ],
         ),
       ),
     );
