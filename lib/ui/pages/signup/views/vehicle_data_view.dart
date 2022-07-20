9c369c32ce0760e079ba15ce0972dc272880674f
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-07-20 10:03:32 -0300

    Adding vehicle color option

--- lib/ui/pages/signup/views/vehicle_data_view.dart
+++ lib/ui/pages/signup/views/vehicle_data_view.dart
@@ -1,7 +1,8 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 import '../../../extension/extension.dart';
+import '../../../mixins/mixins.dart';
 import '../../../utils/utils.dart';
 import '../components/components.dart';
 
@@ -16,22 +17,25 @@ enum VehicleType {
 
 class VehicleDataView extends StatefulWidget {
   final PageController controller;
+
   const VehicleDataView({Key? key, required this.controller}) : super(key: key);
 
   @override
   State<VehicleDataView> createState() => _VehicleDataViewState();
 }
 
-class _VehicleDataViewState extends State<VehicleDataView> {
-  // create some values
-  Color pickerColor = const Color(0xff443a49);
+class _VehicleDataViewState extends State<VehicleDataView> with InputMask {
+  Color screenPickerColor = Colors.red;
+  late Color dialogPickerColor;
+  Color dialogSelectColor = Colors.red;
 
-  Color _currentColor = const Color(0xff443a49);
-
-// ValueChanged<Color> callback
-  void changeColor(Color color) {
-    setState(() => pickerColor = color);
-  }
+  static const Color guidePrimary = Colors.white70;
+  static const Color guidePrimaryVariant = Colors.grey;
+  static const Color guideSecondary = Colors.black;
+  static const Color guideRed = Color(0xFFB00020);
+  static const Color blue = Color(0xFF174378);
+  static const Color guideSecondaryVariant = Color(0xFF018786);
+  static const Color guidePink = Color(0xFFCF6679);
 
   VehicleType? _groupValue;
 
@@ -39,52 +43,22 @@ class _VehicleDataViewState extends State<VehicleDataView> {
     return (value) => setState(() => _groupValue = value!);
   }
 
-  Widget buildColorPicker(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Container(
-              height: 50,
-              width: 10,
-              color: _currentColor,
-            ),
-            const SizedBox(width: 8),
-            Text(
-              context.loc.vehicleColor,
-            ),
-          ],
-        ),
-        TextButton(
-          child: Text(context.loc.selectButton),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: BlockPicker(
-                    useInShowDialog: true,
-                    pickerColor: Colors.white,
-                    onColorChanged: (color) {
-                      setState(() => _currentColor = color);
-                    },
-                  ),
-                  actions: [
-                    TextButton(
-                      child: Text(context.loc.saveButton),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
-      ],
-    );
+  final Map<ColorSwatch<Object>, String> colorsNameMap = <ColorSwatch<Object>, String>{
+    ColorTools.createPrimarySwatch(guidePrimary): 'White',
+    ColorTools.createPrimarySwatch(guidePrimaryVariant): 'Grey',
+    ColorTools.createAccentSwatch(guideSecondary): 'Guide Teal',
+    ColorTools.createAccentSwatch(guideSecondaryVariant): 'Guide Teal Variant',
+    ColorTools.createPrimarySwatch(guideRed): 'Red',
+    ColorTools.createPrimarySwatch(guidePink): 'Pink',
+    ColorTools.createPrimarySwatch(blue): 'Blue',
+  };
+
+  @override
+  void initState() {
+    screenPickerColor = Colors.blue;
+    dialogPickerColor = Colors.red;
+    dialogSelectColor = const Color(0xFFA239CA);
+    super.initState();
   }
 
   @override
@@ -96,10 +70,12 @@ class _VehicleDataViewState extends State<VehicleDataView> {
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.8,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   context.loc.vehicleData,
                   style: Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
                 ),
                 Text(context.loc.vehicleType),
                 Column(
@@ -170,14 +146,22 @@ class _VehicleDataViewState extends State<VehicleDataView> {
                     label: Text(context.loc.vehicleModel),
                   ),
                 ),
-                //TODO: Colocar a cor do veiculo
+                const SizedBox(height: 16),
+                Text(
+                  context.loc.vehicleColor,
+                  textAlign: TextAlign.start,
+                ),
                 const SizedBox(height: 8),
                 buildColorPicker(context),
+
+                const SizedBox(height: 8),
+                // buildColorPicker(context),
                 TextFormField(
                   decoration: InputDecoration(
-                    hintText: context.loc.licensePlate,
+                    hintText: 'ABC1D234 or ABC-1234',
                     label: Text(context.loc.licensePlate),
                   ),
+                  inputFormatters: [licensePlateMaskFormatter],
                 ),
               ],
             ),
@@ -191,6 +175,72 @@ class _VehicleDataViewState extends State<VehicleDataView> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildColorPicker(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ColorPicker(
+          hasBorder: true,
+          elevation: 8,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          padding: EdgeInsets.zero,
+          color: screenPickerColor,
+          enableShadesSelection: false,
+          maxRecentColors: 5,
+          customColorSwatchesAndNames: colorsNameMap,
+          pickersEnabled: const <ColorPickerType, bool>{
+            ColorPickerType.accent: false,
+            ColorPickerType.custom: true,
+            ColorPickerType.primary: false,
+          },
+          onColorChanged: (Color color) => setState(() => screenPickerColor = color),
+        ),
+        const SizedBox(width: 4),
+        ColorIndicator(
+            hasBorder: true,
+            selectedIcon: dialogSelectColor == screenPickerColor ? Icons.check : Icons.add,
+            isSelected: true,
+            width: 40,
+            height: 40,
+            color: dialogSelectColor,
+            //elevation: 1,
+            onSelectFocus: false,
+            onSelect: () async {
+              final Color newColor = await showColorPickerDialog(
+                context,
+                dialogSelectColor,
+                title: Text(context.loc.vehicleColor, style: Theme.of(context).textTheme.headline6),
+                width: 40,
+                height: 40,
+                borderRadius: 10,
+                wheelDiameter: 165,
+                pickersEnabled: <ColorPickerType, bool>{
+                  ColorPickerType.wheel: true,
+                  ColorPickerType.accent: false,
+                  ColorPickerType.primary: false,
+                },
+                copyPasteBehavior: const ColorPickerCopyPasteBehavior(
+                  copyButton: false,
+                  pasteButton: false,
+                  longPressMenu: false,
+                ),
+                actionButtons: const ColorPickerActionButtons(
+                  okButton: true,
+                  closeButton: true,
+                  dialogActionButtons: true,
+                ),
+              );
+
+              setState(() {
+                dialogSelectColor = newColor;
+                screenPickerColor = newColor;
+              });
+            }),
+      ],
     );
   }
 
