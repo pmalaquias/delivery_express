3a5a7d3a51035a12694fd5586ef77c5095fe6012
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-07-20 10:08:40 -0300

    Adding new license plate mask

--- lib/ui/mixins/input_mask.dart
+++ lib/ui/mixins/input_mask.dart
@@ -1,3 +1,4 @@
+import 'package:easy_mask/easy_mask.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 mixin InputMask {
@@ -35,5 +36,11 @@ mixin InputMask {
     mask: '##.######-##',
     filter: {"#": RegExp(r'[0-9]')},
     type: MaskAutoCompletionType.lazy,
+  );
+
+  TextInputMask licensePlateMaskFormatter = TextInputMask(
+    mask: ['AAA9A99', 'AAA-9999'],
+    maxPlaceHolders: 5,
+    reverse: false,
   );
 }
