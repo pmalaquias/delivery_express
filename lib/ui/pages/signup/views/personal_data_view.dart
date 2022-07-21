2e1089d1f21cb003cd1d9efe7bfa1ed8d2330f0b
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2022-07-21 11:45:58 -0300

    Remove expanded Widget

--- lib/ui/pages/signup/views/personal_data_view.dart
+++ lib/ui/pages/signup/views/personal_data_view.dart
@@ -17,86 +17,85 @@ class PersonalDataDeliverymanView extends StatefulWidget {
 class _PersonalDataDeliverymanViewState extends State<PersonalDataDeliverymanView> with InputMask {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.8,
-              child: Column(
-                children: [
-                  Text(
-                    context.loc.personalData,
-                    style: Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 32),
-                  InkWell(
-                    onTap: () => dialogAddNewImage(context),
-                    borderRadius: BorderRadius.circular(50),
-                    hoverColor: AppColors.seccondRed,
-                    radius: 150,
-                    child: const CircleAvatar(
-                      maxRadius: 60,
-                      child: Icon(
-                        Icons.person_add,
-                        color: Colors.white,
-                        size: 72,
-                      ),
-                      backgroundColor: AppColors.primaryRed,
-                    ),
-                    splashColor: AppColors.seccondRed.withOpacity(0.2),
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    //controller: _emailTextController,
-                    decoration: InputDecoration(
-                      hintText: context.loc.fullName,
-                      label: Text(context.loc.fullName),
-                    ),
-                    keyboardType: TextInputType.name,
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: '15/01/2000',
-                      label: Text(context.loc.birthDate),
-                    ),
-                    keyboardType: TextInputType.datetime,
-                    inputFormatters: [birthDataMaskFormatter],
-                    validator: (String? value) => dateValidator(value, context),
-                  ),
-                  const SizedBox(height: 8),
-                  TextFormField(
-                    //controller: _emailTextController,
-                    decoration: InputDecoration(
-                      hintText: '999.999.999-99',
-                      label: Text(context.loc.cpf),
-                    ),
-                    keyboardType: TextInputType.name,
-                    inputFormatters: [cpfMaskFormatter],
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: '(00) 0 0000-0000',
-                      label: Text(context.loc.phoneNumber),
-                    ),
-                    keyboardType: TextInputType.phone,
-                    inputFormatters: [cellPhoneMaskFormatter],
-                  ),
-                  const SizedBox(height: 32),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8,
+            child: Column(
               children: [
-                ButtonCancel(function: pageControllerCancel),
-                ButtonContinue(function: pageControllerContinue),
+                Text(
+                  context.loc.personalData,
+                  style: Theme.of(context).textTheme.headline5!.copyWith(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 32),
+                InkWell(
+                  onTap: () => dialogAddNewImage(context),
+                  borderRadius: BorderRadius.circular(50),
+                  hoverColor: AppColors.seccondRed,
+                  radius: 150,
+                  child: const CircleAvatar(
+                    maxRadius: 60,
+                    child: Icon(
+                      Icons.person_add,
+                      color: Colors.white,
+                      size: 72,
+                    ),
+                    backgroundColor: AppColors.primaryRed,
+                  ),
+                  splashColor: AppColors.seccondRed.withOpacity(0.2),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  //controller: _emailTextController,
+                  decoration: InputDecoration(
+                    hintText: context.loc.fullName,
+                    label: Text(context.loc.fullName),
+                  ),
+                  keyboardType: TextInputType.name,
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: '15/01/2000',
+                    label: Text(context.loc.birthDate),
+                  ),
+                  keyboardType: TextInputType.datetime,
+                  inputFormatters: [birthDataMaskFormatter],
+                  validator: (String? value) => dateValidator(value, context),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  //controller: _emailTextController,
+                  decoration: InputDecoration(
+                    hintText: '999.999.999-99',
+                    label: Text(context.loc.cpf),
+                  ),
+                  keyboardType: TextInputType.name,
+                  inputFormatters: [cpfMaskFormatter],
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: '(00) 0 0000-0000',
+                    label: Text(context.loc.phoneNumber),
+                  ),
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [cellPhoneMaskFormatter],
+                ),
+                const SizedBox(height: 32),
               ],
             ),
-          ],
-        ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ButtonCancel(function: pageControllerCancel),
+              ButtonContinue(function: pageControllerContinue),
+            ],
+          ),
+        ],
       ),
     );
   }
