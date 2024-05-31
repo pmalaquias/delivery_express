f7ee9f33ffe59456575b82bb7790f46e4c60f5f5
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-05-31 13:44:40 -0300

    update: gradle

--- android_project/app/build.gradle.kts
+++ android_project/app/build.gradle.kts
@@ -56,11 +56,13 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0-RC")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
 
     val navVersion = "2.7.7"
+    val composeVersion = "1.6.7"
 
 
-    implementation("androidx.graphics:graphics-core:1.0.0-rc01")
+    implementation("androidx.graphics:graphics-core:1.0.0")
     implementation("androidx.graphics:graphics-path:1.0.1")
     implementation("androidx.graphics:graphics-shapes:1.0.0-beta01")
     implementation("com.google.accompanist:accompanist-pager:0.35.0-alpha")
@@ -68,19 +70,19 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.compose.material:material-icons-extended-android:1.6.7")
+    implementation("androidx.compose.material:material-icons-extended-android:$composeVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta02")
 }
