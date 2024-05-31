0ea9248917692e5d3fb051378ac59c8181fdf82e
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-05-31 13:46:10 -0300

    feat: Add SignupUiState data class for signup personal data page

---
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/SignupUiState.kt
@@ -0,0 +1,10 @@
+package com.pmalaquias.deliveryexpress.presentation.ui.pages.signup
+
+data class SignupPersonalDataUiState(
+    val name: String = "",
+    val birthDate: String = "",
+    val id: Int  = 0,
+    val phone: Int = 0,
+    val photo: String = "",
+    val isLoading: Boolean = false,
+)
