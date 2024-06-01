516c8629cec5f80df007438a96f158e5638b43ac
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-01 13:59:26 -0300

    feat: Add commercialName field to SignupPersonalDataUiState

--- android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/SignupUiState.kt
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/SignupUiState.kt
@@ -2,8 +2,9 @@ package com.pmalaquias.deliveryexpress.presentation.ui.pages.signup
 
 data class SignupPersonalDataUiState(
     val name: String = "",
+    val commercialName: String = "",
     val birthDate: String = "",
-    val id: Int  = 0,
+    val id: Int = 0,
     val phone: Int = 0,
     val photo: String = "",
     val isLoading: Boolean = false,
