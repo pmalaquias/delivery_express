2f30d540b5af8cc4a52e4d3b1800a933cb6fe8d0
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-02 10:57:08 -0300

    Refactor use ViewModel

--- android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/SignUpAccessDataPage.kt
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/SignUpAccessDataPage.kt
@@ -15,35 +15,46 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pmalaquias.deliveryexpress.R
 import com.pmalaquias.deliveryexpress.presentation.ui.pages.signup.components.AppBarClient
 import com.pmalaquias.deliveryexpress.presentation.ui.pages.signup.components.AppBarDeliveryPerson
 import com.pmalaquias.deliveryexpress.presentation.ui.theme.AppTheme
+import com.pmalaquias.deliveryexpress.presentation.viewModel.signup.SignUpAccessDataViewModel
 
+/**
+ * Composable function that represents the SignUpAccessDeliveryPersonDataPage.
+ *
+ * This page is used for the delivery person to enter their access data during the signup process.
+ * It contains text fields for email, confirm email, password, and confirm password.
+ * It also contains a back button and a continue button.
+ *
+ * @param onCancelButtonClicked A lambda function that is called when the back button is clicked.
+ * @param onNextButtonClicked A lambda function that is called when the continue button is clicked.
+ * @param modifier A Modifier that is applied to the root layout of this composable.
+ */
 @Composable
 fun SignUpAccessDeliveryPersonDataPage(
     onCancelButtonClicked: () -> Unit = {},
     onNextButtonClicked: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
-    var text by rememberSaveable { mutableStateOf("") }
+    // ViewModel for this page
+    val viewModel: SignUpAccessDataViewModel = viewModel()
 
-
+    // Scaffold for the layout of this page
     Scaffold(
         Modifier,
         topBar = { AppBarDeliveryPerson() }
     ) { innerPadding ->
 
+        // Main layout of this page
         Column(
             modifier = modifier
                 .padding(innerPadding)
@@ -54,7 +65,7 @@ fun SignUpAccessDeliveryPersonDataPage(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-
+            // Layout for the text fields
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -66,36 +77,36 @@ fun SignUpAccessDeliveryPersonDataPage(
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.email,
+                    onValueChange = { viewModel.onEmailChange(it) },
                     label = { Text(stringResource(id = R.string.email_label)) },
                     modifier = modifier.fillMaxWidth(),
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.confirmEmail,
+                    onValueChange = { viewModel.onConfirmEmailChange(it) },
                     label = { Text(stringResource(id = R.string.confirm_email)) },
                     modifier = modifier.fillMaxWidth(),
                 )
-                Spacer(modifier = modifier.padding(8.dp))
+                Spacer(modifier = Modifier.padding(8.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.password,
+                    onValueChange = { viewModel.onPasswordChange(it) },
                     label = { Text(stringResource(id = R.string.password_label)) },
                     modifier = modifier.fillMaxWidth(),
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.confirmPassword,
+                    onValueChange = { viewModel.onConfirmPasswordChange(it) },
                     label = { Text(stringResource(id = R.string.confirm_password)) },
                     modifier = modifier.fillMaxWidth(),
                 )
                 Spacer(modifier = Modifier.size(16.dp))
-
-
             }
+
+            // Layout for the back and continue buttons
             Row(
                 modifier = modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
@@ -107,11 +118,9 @@ fun SignUpAccessDeliveryPersonDataPage(
                 }
                 Button(onClick = onNextButtonClicked) {
                     Text(text = stringResource(id = R.string.continue_button))
-
                 }
             }
         }
-
     }
 }
 
@@ -123,20 +132,33 @@ fun SignUpAccessDataPagePreview() {
     }
 }
 
+/**
+ * Composable function that represents the SignUpAccessClientDataPage.
+ *
+ * This page is used for the client to enter their access data during the signup process.
+ * It contains text fields for email, confirm email, password, and confirm password.
+ * It also contains a back button and a continue button.
+ *
+ * @param onCancelButtonClicked A lambda function that is called when the back button is clicked.
+ * @param onNextButtonClicked A lambda function that is called when the continue button is clicked.
+ * @param modifier A Modifier that is applied to the root layout of this composable.
+ */
 @Composable
 fun SignUpAccessClientDataPage(
     onCancelButtonClicked: () -> Unit = {},
     onNextButtonClicked: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
-    var text by rememberSaveable { mutableStateOf("") }
+    // ViewModel for this page
+    val viewModel: SignUpAccessDataViewModel = viewModel()
 
-
+    // Scaffold for the layout of this page
     Scaffold(
         Modifier,
         topBar = { AppBarClient() }
     ) { innerPadding ->
 
+        // Main layout of this page
         Column(
             modifier = modifier
                 .padding(innerPadding)
@@ -147,7 +169,7 @@ fun SignUpAccessClientDataPage(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-
+            // Layout for the text fields
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -159,29 +181,29 @@ fun SignUpAccessClientDataPage(
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.email,
+                    onValueChange = { viewModel.onEmailChange(it) },
                     label = { Text(stringResource(id = R.string.email_label)) },
                     modifier = modifier.fillMaxWidth(),
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.confirmEmail,
+                    onValueChange = { viewModel.onConfirmEmailChange(it) },
                     label = { Text(stringResource(id = R.string.confirm_email)) },
                     modifier = modifier.fillMaxWidth(),
                 )
-                Spacer(modifier = modifier.padding(8.dp))
+                Spacer(modifier = Modifier.padding(8.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.password,
+                    onValueChange = { viewModel.onPasswordChange(it) },
                     label = { Text(stringResource(id = R.string.password_label)) },
                     modifier = modifier.fillMaxWidth(),
                 )
                 Spacer(modifier = Modifier.size(16.dp))
                 TextField(
-                    value = text,
-                    onValueChange = { /*TODO*/ },
+                    value = viewModel.confirmPassword,
+                    onValueChange = { viewModel.onConfirmPasswordChange(it)},
                     label = { Text(stringResource(id = R.string.confirm_password)) },
                     modifier = modifier.fillMaxWidth(),
                 )
@@ -189,6 +211,7 @@ fun SignUpAccessClientDataPage(
 
 
             }
+            // Layout for the back and continue buttons
             Row(
                 modifier = modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
