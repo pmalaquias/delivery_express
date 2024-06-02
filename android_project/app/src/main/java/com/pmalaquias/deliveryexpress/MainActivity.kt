e91f5e7a28eb38f2f074947a2fc892b2fc7d36e4
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-01 22:19:22 -0300

    feat: Enable edge-to-edge and transparent status bar in MainActivity

--- android_project/app/src/main/java/com/pmalaquias/deliveryexpress/MainActivity.kt
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/MainActivity.kt
@@ -2,10 +2,13 @@ package com.pmalaquias.deliveryexpress
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.pmalaquias.deliveryexpress.presentation.ui.pages.login.DeliveryApp
 import com.pmalaquias.deliveryexpress.presentation.ui.theme.AppTheme
-
 
 /**
  *   The main activity of the Delivery Express app.
@@ -15,15 +18,21 @@ import com.pmalaquias.deliveryexpress.presentation.ui.theme.AppTheme
  *  @project DeliveryExpress
  *  @author Pedro Igor
  *  @version 1.0
-* */
+ * */
 class MainActivity : ComponentActivity() {
     /**
      * Initializes the activity and sets up the UI elements.
      */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             AppTheme {
+                // Add this line to enable edge-to-edge
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.light(Color.Transparent.toArgb(), Color.Transparent.toArgb())
+                )
                 // A surface container using the 'background' color from the theme
                 DeliveryApp()
 
