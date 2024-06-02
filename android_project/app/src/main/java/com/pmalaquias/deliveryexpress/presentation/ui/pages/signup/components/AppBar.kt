fbc994ac299261793c503382cc38080aecf5d17d
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-01 22:28:46 -0300

    Refactor AppBar composable functions for signup screens

--- android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/components/AppBar.kt
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/pages/signup/components/AppBar.kt
@@ -10,6 +10,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.pmalaquias.deliveryexpress.R
 
+
+
+/**
+ * A composable function that displays an AppBar for the Delivery Person registration screen.
+ *
+ * This AppBar displays a centered text, which is a string resource with the id `R.string.register_delivery_person`.
+ * The text uses the `headlineLarge` style from the current Material Theme.
+ * The text is also set to fill the maximum width available, aligning its content to the center.
+ *
+ * This composable does not take any parameters and does not return any value.
+ * */
 @Composable
 fun AppBarDeliveryPerson() {
     Text(
@@ -27,6 +38,15 @@ fun AppBarDeliveryPersonPreview() {
     AppBarDeliveryPerson()
 }
 
+/**
+ * A composable function that displays an AppBar for the Client registration screen.
+ *
+ * This AppBar displays a centered text, which is a string resource with the id `R.string.register_client`.
+ * The text uses the `headlineLarge` style from the current Material Theme.
+ * The text is also set to fill the maximum width available, aligning its content to the center.
+ *
+ * This composable does not take any parameters and does not return any value.
+ * */
 @Composable
 fun AppBarClient() {
     Text(
