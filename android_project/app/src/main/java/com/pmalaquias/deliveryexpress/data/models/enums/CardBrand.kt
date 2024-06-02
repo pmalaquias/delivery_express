97068230e2a54f9015203f1ee4c14b0da248a15d
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-02 16:33:57 -0300

    Add CardBrand enum for representing different brands of cards

---
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/data/models/enums/CardBrand.kt
@@ -0,0 +1,27 @@
+package com.pmalaquias.deliveryexpress.data.models.enums
+
+
+/**
+ * Enum class representing different brands of cards.
+ *
+ * @property VISA Represents the Visa card brand.
+ * @property MASTERCARD Represents the Mastercard card brand.
+ * @property ELO Represents the Elo card brand.
+ * @property HIPERCARD Represents the Hipercard card brand.
+ * @property AMEX Represents the American Express card brand.
+ * @property DISCOVER Represents the Discover card brand.
+ * @property JCB Represents the JCB card brand.
+ * @property DINERS_CLUB Represents the Diners Club card brand.
+ * @property UNKNOWN Represents an unknown card brand.
+ */
+enum class CardBrand {
+    VISA,
+    MASTERCARD,
+    ELO,
+    HIPERCARD,
+    AMEX,
+    DISCOVER,
+    JCB,
+    DINERS_CLUB,
+    UNKNOWN
+}
