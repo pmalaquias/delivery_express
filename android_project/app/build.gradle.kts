718f5fc1aabb5c35a4caf25bc85c2c43689a9442
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-10 13:19:35 -0300

    chore: Update Google Maps Compose dependency to version 5.0.3

--- android_project/app/build.gradle.kts
+++ android_project/app/build.gradle.kts
@@ -56,11 +56,11 @@ android {
 }
 
 dependencies {
-    implementation("com.google.maps.android:maps-compose:5.0.2")
+    implementation("com.google.maps.android:maps-compose:5.0.3")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("androidx.wear.compose:compose-material:1.3.1")
     implementation("com.google.code.gson:gson:2.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.compose.material3:material3-android:1.2.1")
@@ -83,9 +83,9 @@ dependencies {
     implementation("androidx.compose.animation:animation:1.6.7")
     implementation("javax.inject:javax.inject:1")
     implementation("com.google.android.datatransport:transport-runtime:3.3.0")
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0-RC")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
-    //implementation("com.squareup.retrofit:retrofit-client")
 
     val navVersion = "2.7.7"
     val composeVersion = "1.6.7"
@@ -114,4 +114,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta02")
+
+
 }
