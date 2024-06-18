019623347a5011e6b91737b6b8e54dc0b0bd7098
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-18 09:23:41 -0300

    refactor: Update theme to use transparent navigation and status bar

--- android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/theme/Theme.kt
+++ android_project/app/src/main/java/com/pmalaquias/deliveryexpress/presentation/ui/theme/Theme.kt
@@ -2,15 +2,17 @@ package com.pmalaquias.deliveryexpress.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -259,13 +261,13 @@ data class ColorFamily(
 val unspecified_scheme = ColorFamily(
     Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
 )
-/*
+
 @Composable
-fun DeliveryExpressTheme(
+fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    dynamicColor: Boolean = false,
+    content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -280,8 +282,10 @@ fun DeliveryExpressTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.onPrimary.toArgb()
+            window.navigationBarColor = colorScheme.onPrimary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+
         }
     }
 
@@ -291,43 +295,4 @@ fun DeliveryExpressTheme(
         content = content
     )
 }
-*/
 
-@Composable
-fun AppTheme(
-    useDarkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-) {
-    val colors = if (!useDarkTheme) {
-        lightScheme
-    } else {
-        darkScheme
-    }
-
-
-    val systemUiController = rememberSystemUiController()
-    if(useDarkTheme){
-        systemUiController.setSystemBarsColor(
-            color = Color.Transparent
-        )
-    }else{
-        systemUiController.setSystemBarsColor(
-            color = Color.White
-        )
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colors.background.toArgb()
-            //WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-            window.navigationBarColor = colors.onBackground.toArgb()
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colors,
-        typography = Typography,
-        content = content
-    )
-}
