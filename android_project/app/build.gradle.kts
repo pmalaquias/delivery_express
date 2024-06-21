f36a9ace75e819370a6055cb107b8fda39c9f67b
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-06-21 18:02:43 -0300

    chore: Update androidx.lifecycle dependencies to version 2.8.2

--- android_project/app/build.gradle.kts
+++ android_project/app/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     implementation("com.google.android.datatransport:transport-runtime:3.3.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("dev.chrisbanes.haze:haze:0.7.2")
-    implementation("dev.chrisbanes.haze:haze-materials:0.7.1")
+    implementation("dev.chrisbanes.haze:haze-materials:0.7.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0-RC")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
