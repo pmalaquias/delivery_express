594ee2bf9cd4c9eee1055497a24a42e8a26dfa56
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2024-08-08 17:17:34 -0300

    chore: Update dependencies to latest versions

--- android_project/app/build.gradle.kts
+++ android_project/app/build.gradle.kts
@@ -56,17 +56,17 @@ android {
 }
 
 dependencies {
-    implementation("com.google.maps.android:maps-compose:5.0.4")
+    implementation("com.google.maps.android:maps-compose:6.1.1")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("androidx.wear.compose:compose-material:1.3.1")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
-    implementation("androidx.compose.material3:material3-android:1.2.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.compose.material3:material3-android:1.3.0-beta05")
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
-    implementation("io.coil-kt:coil:2.6.0")
-    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt:coil:2.7.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
 
 
@@ -84,8 +84,8 @@ dependencies {
     implementation("javax.inject:javax.inject:1")
     implementation("com.google.android.datatransport:transport-runtime:3.3.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("dev.chrisbanes.haze:haze:0.7.2")
-    implementation("dev.chrisbanes.haze:haze-materials:0.7.2")
+    implementation("dev.chrisbanes.haze:haze:0.7.3")
+    implementation("dev.chrisbanes.haze:haze-materials:0.7.3")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0-RC")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
@@ -95,19 +95,19 @@ dependencies {
 
     implementation("androidx.graphics:graphics-core:1.0.0")
     implementation("androidx.graphics:graphics-path:1.0.1")
-    implementation("androidx.graphics:graphics-shapes:1.0.0-beta01")
+    implementation("androidx.graphics:graphics-shapes:1.0.0-rc01")
     implementation("com.google.accompanist:accompanist-pager:0.34.0")
     implementation("com.google.accompanist:accompanist-pager-indicators:0.34.0")
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-graphics:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.3.0-beta04")
+    implementation("androidx.compose.material3:material3:1.3.0-beta05")
     implementation("androidx.compose.material:material-icons-extended-android:$composeVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
@@ -115,7 +115,7 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta04")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.0-beta07")
 
 
 }
