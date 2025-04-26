24d33100bd167d4ba6dca1ddd4b4dffc4ef6e43e
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-04-26 15:17:13 -0300

    fix: Update dependencies and enforce Kotlin stdlib version

--- android_project/app/build.gradle.kts
+++ android_project/app/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+configurations.all {
+    resolutionStrategy {
+        force("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    }
+}
+
 android {
     namespace = "com.pmalaquias.deliveryexpress"
     compileSdk = 35
@@ -57,13 +63,13 @@ android {
 
 dependencies {
     implementation("com.google.maps.android:maps-compose:6.1.1")
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
-    implementation("androidx.wear.compose:compose-material:1.4.0")
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("androidx.wear.compose:compose-material:1.4.1")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.compose.material3:material3-android:1.3.1")
+    implementation("androidx.compose.material3:material3-android:1.3.2")
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
     implementation("io.coil-kt:coil:2.7.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
@@ -80,42 +86,42 @@ dependencies {
     implementation ("com.squareup.okhttp3:okhttp:4.12.0")
     //moshi
     implementation ("com.squareup.moshi:moshi-kotlin:1.15.1")
-    implementation("androidx.compose.animation:animation:1.7.5")
+    implementation("androidx.compose.animation:animation:1.8.0")
     implementation("javax.inject:javax.inject:1")
     implementation("com.google.android.datatransport:transport-runtime:4.0.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("dev.chrisbanes.haze:haze:0.7.3")
     implementation("dev.chrisbanes.haze:haze-materials:0.7.3")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0-RC")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
-    val navVersion = "2.8.4"
+    val navVersion = "2.8.9"
     val composeVersion = "1.7.5"
 
 
-    implementation("androidx.graphics:graphics-core:1.0.2")
+    implementation("androidx.graphics:graphics-core:1.0.3")
     implementation("androidx.graphics:graphics-path:1.0.1")
     implementation("androidx.graphics:graphics-shapes:1.0.1")
     implementation("com.google.accompanist:accompanist-pager:0.34.0")
     implementation("com.google.accompanist:accompanist-pager-indicators:0.34.0")
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.04.01"))
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-graphics:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.compose.material:material-icons-extended-android:$composeVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.8.0")
 
 
 }
