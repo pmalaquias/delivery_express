357986e6b4ad697119cf0bb6aeb6b4bbf6d0d5b1
Pedro Igor <40576393+pmalaquias@users.noreply.github.com>
2025-04-27 17:52:43 -0300

    fix: Update kotlinx-coroutines-test dependency to version 1.10.2

--- android_project/app/build.gradle.kts
+++ android_project/app/build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("dev.chrisbanes.haze:haze:0.7.3")
     implementation("dev.chrisbanes.haze:haze-materials:0.7.3")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
     val navVersion = "2.8.9"
